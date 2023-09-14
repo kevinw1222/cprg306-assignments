384f950c5c4be515e28a03e869e7f94c5769d905
kevinw1222 <homingkevin.wong@edu.sait.ca>
2023-09-14 15:13:08 -0600

    Update page.js

--- app/week2/page.js
+++ app/week2/page.js
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function MyPage() {
   return (
     <main>
+      <Link href="/">Home</Link>
       <h2>My Shopping List</h2>
       <StudentInfo />
-      <Link href="/">Home</Link>
     </main>
   );
 }
