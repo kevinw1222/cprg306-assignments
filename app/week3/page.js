650e7fca50c93e2d2f2a3f0e429beedc6ca33ec7
kevinw1222 <homingkevin.wong@edu.sait.ca>
2023-09-19 16:11:57 -0600

    Update page.js

--- app/week3/page.js
+++ app/week3/page.js
@@ -3,8 +3,21 @@ import Item from "./items";
 
 export default function Page() {
   return (
-    <main class="bg-white">
-      <p className="text-3xl font-bold p-2 m-4">Shopping List</p>
+    <main>
+      <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <span class="font-semibold text-xl tracking-tight">
+            Shopping List
+          </span>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow">
+            <button className="px-4 py-2 text-white bg-orange-500 rounded hover:bg-orange-400">
+              <Link href="/">Home</Link>
+            </button>
+          </div>
+        </div>
+      </nav>
       <ItemList />
     </main>
   );
