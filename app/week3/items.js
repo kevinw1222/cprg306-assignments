008803231b2a735412d2f5d1b9ebd44a72247bc9
kevinw1222 <homingkevin.wong@edu.sait.ca>
2023-09-19 14:15:02 -0600

    Update items.js

--- app/week3/items.js
+++ app/week3/items.js
@@ -1,9 +1,9 @@
 export default function Item({ name, quantity, category }) {
   return (
     <main>
-      <li class="p-2 m-4 bg-orange-500 max-w-sm rounded-lg hover:bg-orange-300">
-          <p class="text-xl font-bold">{name}</p>
-          <p class="text-md">
+      <li class="p-2 m-4 bg-orange-500 max-w-sm rounded-lg hover:bg-orange-400">
+        <p class="text-xl font-bold">{name}</p>
+        <p class="text-md">
           By {quantity} in {category}
         </p>
       </li>
