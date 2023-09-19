7f6cc3ce2a733013f98fe9705c1a4d6fabeeff6f
kevinw1222 <homingkevin.wong@edu.sait.ca>
2023-09-19 16:12:43 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,10 +3,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <Link href="/week2">Week 2</Link>
+    <main class="m-4">
       <h1>CPRG-306-F: Web Dev 2 - Assignments</h1>
       <StudentInfo />
+      <ul class="m-4">
+        <button className="px-4 py-2 text-white bg-orange-500 rounded hover:bg-orange-400">
+          <Link href="/week2">Week 2</Link>
+        </button>
+      </ul>
+      <ul class="m-4">
+        <button className="px-4 py-2 text-white bg-orange-500 rounded hover:bg-orange-400">
+          <Link href="/week3">Week 3</Link>
+        </button>
+      </ul>
     </main>
   );
 }
