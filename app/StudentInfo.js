4edc0c3f564ac2500054de8205ac1bd28e8baaa1
kevinw1222 <homingkevin.wong@edu.sait.ca>
2023-09-19 21:06:47 -0600

    Update StudentInfo.js

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
   return (
     <main>
       <h2>Name: Kevin Wong</h2>
-      <h2>Class: CPRG-306-F Web Dev 2</h2>
+      <h2>Class: CPRG-306-F</h2>
       <p>
         GitHub:{" "}
         <Link class="underline" href="https://www.kevinw1222@github.com">kevinw1222</Link>
